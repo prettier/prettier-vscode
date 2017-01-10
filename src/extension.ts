ce0412fe987034df9ac786f7170f98c46bcbee9a
Esben Petersen <esbenspetersen@gmail.com>
2017-01-10 20:34:54 +0100

    init

---
+++ src/extension.ts
@@ -0,0 +1,74 @@
+'use strict';
+
+import { commands, ExtensionContext, Range, Position, TextEdit, window, workspace } from 'vscode';
+const prettier = require('prettier')
+
+export function activate(context: ExtensionContext) {
+
+    let eventDisposable = workspace.onWillSaveTextDocument((e: workspace.TextDocumentWillSaveEvent) => {
+        const document = e.document;
+
+        if (!document.isDirty) {
+            return;
+        }
+
+        const config = workspace.getConfiguration('prettier');
+        const formatOnSave = config.formatOnSave;
+        if (!formatOnSave) {
+            return;
+        }  
+
+        e.waitUntil(new Promise(resolve => {
+            const prettified = format(document, null);
+
+            const rangeObj = new Range(0, 0, document.lineCount, 0);
+            const edit = TextEdit.replace(rangeObj, prettified);
+
+            resolve([edit]);
+        })
+    })
+
+    let disposable = commands.registerCommand('prettier.format', () => {
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        const selection = editor.selection;
+
+        const prettified = format(editor.document, selection);
+
+        editor.edit((editBuilder) => {
+            const rangeObj = new Range(
+                selection.start.line,
+                selection.start.character,
+                selection.end.line,
+                selection.end.character
+            );
+            
+            editBuilder.replace(rangeObj, prettified);
+        })
+        
+        
+    });
+
+    context.subscriptions.push(eventDisposable);
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+const format = (document, selection = null) => {
+    const text = document.getText(selection)
+
+    try {
+        var transformed = prettier.format(text)
+    } catch (e) {
+        console.log("Error transforming using prettier:", e);
+        transformed = text;
+    }
+
+    return transformed
+}
