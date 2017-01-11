f6cd79be4c28d4254ef8e8f0b039d3a5ae389fef
Cole Kennedy <colek42@users.noreply.github.com>
2017-01-11 18:14:56 +0200

    Adds config params (#3)
    * add configs
    
    * added contrib, white space
    
    * Clean Up
    - Added vscode to gitignore
    
    * issues with the vscode dir, settings files dont match up.  Added to git ignore
    
    * add settings.json back in
    
    * Update .gitignore

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import { commands, ExtensionContext, Range, Position, TextEdit, window, workspac
 const prettier = require('prettier')
 
 export function activate(context: ExtensionContext) {
-
     const eventDisposable = (workspace as any).onWillSaveTextDocument(e => {
         const document = e.document;
 
@@ -20,7 +19,6 @@ export function activate(context: ExtensionContext) {
 
         e.waitUntil(new Promise(resolve => {
             const prettified = format(document, null);
-
             const rangeObj = new Range(0, 0, document.lineCount, 0);
             const edit = TextEdit.replace(rangeObj, prettified);
 
@@ -35,7 +33,6 @@ export function activate(context: ExtensionContext) {
         }
 
         const selection = editor.selection;
-
         const prettified = format(editor.document, selection);
 
         editor.edit((editBuilder) => {
@@ -45,13 +42,9 @@ export function activate(context: ExtensionContext) {
                 selection.end.line,
                 selection.end.character
             );
-            
             editBuilder.replace(rangeObj, prettified);
         })
-        
-        
     });
-
     context.subscriptions.push(eventDisposable);
     context.subscriptions.push(disposable);
 }
@@ -59,12 +52,27 @@ export function activate(context: ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
+const config = workspace.getConfiguration('prettier');
+
+const printWidth = (config as any).printWidth;
+const tabWidth = (config as any).tabWidth;
+const useFlowParser = (config as any).useFlowParser;
+const singleQuote = (config as any).singleQuote;
+const trailingComma = (config as any).trailingComma;
+const bracketSpacing = (config as any).bracketSpacing;
 
 const format = (document, selection = null) => {
     const text = document.getText(selection)
 
     try {
-        var transformed = prettier.format(text)
+        var transformed = prettier.format(text, {
+            printWidth: printWidth,
+            tabWidth: tabWidth,
+            useFlowParser: useFlowParser,
+            singleQuote: singleQuote,
+            trailingComma: trailingComma,
+            bracketSpacing: bracketSpacing
+        });
     } catch (e) {
         console.log("Error transforming using prettier:", e);
         transformed = text;
