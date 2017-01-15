e85af4a6135c21ff6f2c3dffc02d5e2131b4e713
Cyril Junod <cyril.junod@gmail.com>
2017-01-15 15:37:44 +0100

    use registerDocumentRangeFormattingEditProvider
    + javascriptreact lang support

---
+++ src/PrettierEditProvider.ts
@@ -0,0 +1,52 @@
+import {
+    workspace,
+    DocumentRangeFormattingEditProvider,
+    Range,
+    TextDocument,
+    FormattingOptions,
+    CancellationToken,
+    TextEdit
+} from 'vscode';
+
+const prettier = require('prettier');
+
+interface PrettierConfig {
+    printWidth: number,
+    tabWidth: number,
+    useFlowParser: boolean,
+    singleQuote: boolean,
+    trailingComma: boolean,
+    bracketSpacing: boolean,
+    formatOnSave: boolean
+}
+
+function format(text: string): string {
+    const config: PrettierConfig = workspace.getConfiguration('prettier') as any;
+    let transformed: string;
+    try {
+        return prettier.format(text, {
+            printWidth: config.printWidth,
+            tabWidth: config.tabWidth,
+            useFlowParser: config.useFlowParser,
+            singleQuote: config.singleQuote,
+            trailingComma: config.trailingComma,
+            bracketSpacing: config.bracketSpacing
+        });
+    } catch (e) {
+        console.log("Error transforming using prettier:", e);
+        return text;
+    }
+}
+class PrettierEditProvider implements DocumentRangeFormattingEditProvider {
+    provideDocumentRangeFormattingEdits(
+        document: TextDocument,
+        range: Range,
+        options: FormattingOptions,
+        token: CancellationToken
+    ): TextEdit[] {
+        return [TextEdit.replace(range, format(document.getText(range)))];
+    }
+}
+
+export default PrettierEditProvider;
+export { PrettierConfig, format };
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,22 @@
-'use strict';
+import {
+    commands,
+    languages,
+    ExtensionContext,
+    DocumentSelector,
+    Range,
+    Position,
+    TextEdit,
+    TextDocument,
+    window,
+    workspace
+} from 'vscode';
+import EditProvider, { format, PrettierConfig } from './PrettierEditProvider';
 
-import { commands, ExtensionContext, Range, Position, TextEdit, window, workspace } from 'vscode';
-const prettier = require('prettier')
-
-interface PrettierConfig {
-    printWidth: number,
-    tabWidth: number,
-    useFlowParser: boolean,
-    singleQuote: boolean,
-    trailingComma: boolean,
-    bracketSpacing: boolean,
-    formatOnSave: boolean
-}
-
-interface Rangeable {
-    end: Position,
-    isEmpty: boolean,
-    start: Position
-}
+const VALID_LANG: DocumentSelector = ['javascript', 'javascriptreact'];
 
 export function activate(context: ExtensionContext) {
     const eventDisposable = (workspace as any).onWillSaveTextDocument(e => {
-        const document = e.document;
+        const document: TextDocument = e.document;
 
         if (!document.isDirty) {
             return;
@@ -34,7 +29,7 @@ export function activate(context: ExtensionContext) {
         }
 
         e.waitUntil(new Promise(resolve => {
-            const prettified = format(document, null);
+            const prettified = format(document.getText());
             const rangeObj = createFullDocumentRange(document)
             const edit = TextEdit.replace(rangeObj, prettified);
 
@@ -50,12 +45,12 @@ export function activate(context: ExtensionContext) {
 
         const document = editor.document
 
-        let selectionOrRange : Rangeable = editor.selection;
+        let selectionOrRange: Range = editor.selection;
         if (selectionOrRange.isEmpty) {
             selectionOrRange = createFullDocumentRange(document)
         }
 
-        const prettified = format(document, selectionOrRange);
+        const prettified = format(document.getText(selectionOrRange));
 
         editor.edit((editBuilder) => {
             const rangeObj = new Range(
@@ -69,6 +64,7 @@ export function activate(context: ExtensionContext) {
     });
     context.subscriptions.push(eventDisposable);
     context.subscriptions.push(disposable);
+    context.subscriptions.push(languages.registerDocumentRangeFormattingEditProvider(VALID_LANG, new EditProvider()));
 }
 
 // this method is called when your extension is deactivated
@@ -76,24 +72,3 @@ export function deactivate() {
 }
 
 const createFullDocumentRange = document => new Range(0, 0, document.lineCount, 0)
-
-const format = (document, selection = null) => {
-    const text = document.getText(selection)
-    const config: PrettierConfig = workspace.getConfiguration('prettier') as any;
-
-    try {
-        var transformed = prettier.format(text, {
-            printWidth: config.printWidth,
-            tabWidth: config.tabWidth,
-            useFlowParser: config.useFlowParser,
-            singleQuote: config.singleQuote,
-            trailingComma: config.trailingComma,
-            bracketSpacing: config.bracketSpacing
-        });
-    } catch (e) {
-        console.log("Error transforming using prettier:", e);
-        transformed = text;
-    }
-
-    return transformed
-}
