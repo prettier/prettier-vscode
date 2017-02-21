6a262f72066f35b08fb814dad62c39152f89ffca
Jacob Parker <jacobparker1992@gmail.com>
2017-02-21 15:23:25 +0000

    Allow disabling formatting when prettier is not installed in the project to assist with editor.saveOnFormat

--- src/PrettierEditProvider.ts
+++ src/PrettierEditProvider.ts
@@ -9,9 +9,13 @@ import {
     TextEdit
 } from 'vscode';
 
+const path = require('path');
+const fs = require('fs');
+
 const prettier = require('prettier');
 
 interface PrettierConfig {
+    onlyFormatWhenInstalled: boolean,
     printWidth: number,
     tabWidth: number,
     useFlowParser: boolean, // deprecated
@@ -46,6 +50,47 @@ function format(text: string): string {
     }
 }
 
+function getPackageJson(fileName: string): any {
+    const stats = fs.statSync(fileName);
+    if (!stats.isDirectory()) return null;
+
+    const packageJsonPath = path.join(fileName, 'package.json');
+    if (!fs.existsSync(packageJsonPath)) return null;
+
+    try {
+        const packageJsonContents = fs.readFileSync(packageJsonPath, 'utf8');
+        return JSON.parse(packageJsonContents);
+    } catch (e) {
+        return null;
+    }
+}
+
+function isInstalledInProject(document: TextDocument): boolean {
+    let currentDir = path.dirname(document.fileName);
+
+    while (path.basename(currentDir) !== '') {
+        const packageJson = getPackageJson(currentDir);
+
+        if (packageJson) {
+            const { devDependencies = {}, dependencies = {} } = packageJson;
+            const isInstalled = ('prettier' in devDependencies) || ('prettier' in dependencies);
+            return isInstalled;
+        }
+
+        currentDir = path.dirname(currentDir);
+    }
+
+    return true;
+}
+
+function shouldFormat(document: TextDocument): boolean {
+    const config: PrettierConfig = workspace.getConfiguration('prettier') as any;
+
+    return (config.onlyFormatWhenInstalled && !document.isUntitled)
+        ? isInstalledInProject(document)
+        : true;
+}
+
 function fullDocumentRange(document: TextDocument): Range {
     const lastLineId = document.lineCount - 1;
     return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
@@ -60,6 +105,7 @@ class PrettierEditProvider implements
         options: FormattingOptions,
         token: CancellationToken
     ): TextEdit[] {
+        if (!shouldFormat(document)) return [];
         return [TextEdit.replace(range, format(document.getText(range)))];
     }
     provideDocumentFormattingEdits(
@@ -67,6 +113,7 @@ class PrettierEditProvider implements
         options: FormattingOptions,
         token: CancellationToken
     ): TextEdit[] {
+        if (!shouldFormat(document)) return [];
         return [TextEdit.replace(fullDocumentRange(document), format(document.getText()))];
     }
 }
