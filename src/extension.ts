8209cde06a729932cf3c1d7ece26223a4d45ca5f
Cyril Junod <cyril.junod@gmail.com>
2018-01-04 12:19:02 +0100

    handle workspaceFolder change
    handle not persisted files

--- src/extension.ts
+++ src/extension.ts
@@ -5,85 +5,123 @@ import {
     DocumentFilter,
     DocumentSelector,
     RelativePattern,
+    WorkspaceFolder,
+    Disposable,
 } from 'vscode';
 import EditProvider from './PrettierEditProvider';
 import { setupErrorHandler, registerDisposables } from './errorHandler';
 import { allEnabledLanguages, allJSLanguages, getConfig } from './utils';
 import configFileListener from './configCacheHandler';
 import ignoreFileHandler from './ignoreFileHandler';
-interface Selector {
+
+interface Selectors {
     rangeLanguageSelector: DocumentSelector;
     languageSelector: DocumentSelector;
 }
-function selectors(): Selector {
+
+let formatterHandler: undefined | Disposable;
+let rangeFormatterHandler: undefined | Disposable;
+function disposeHandlers() {
+    if (formatterHandler) {
+        formatterHandler.dispose();
+    }
+    if (rangeFormatterHandler) {
+        rangeFormatterHandler.dispose();
+    }
+    formatterHandler = undefined;
+    rangeFormatterHandler = undefined;
+}
+
+function selectorsCreator(wf: WorkspaceFolder) {
     const allLanguages = allEnabledLanguages();
     const allRangeLanguages = allJSLanguages();
+    const { disableLanguages } = getConfig(wf.uri);
+    const relativePattern = new RelativePattern(wf, '**');
+    function docFilterForLangs(languages: string[]) {
+        return languages.filter(l => !disableLanguages.includes(l)).map(
+            l =>
+                ({
+                    language: l,
+                    pattern: relativePattern,
+                } as DocumentFilter)
+        );
+    }
+    const languageSelector = docFilterForLangs(allLanguages);
+
+    const rangeLanguageSelector = docFilterForLangs(allRangeLanguages);
+
+    return { languageSelector, rangeLanguageSelector };
+}
+
+function selectors(): Selectors {
+    const allLanguages = allEnabledLanguages();
+    const allRangeLanguages = allJSLanguages();
+    const { disableLanguages } = getConfig();
+    const globalLanguageSelector = allLanguages.filter(
+        l => !disableLanguages.includes(l)
+    );
+    const globalRangeLanguageSelector = allRangeLanguages.filter(
+        l => !disableLanguages.includes(l)
+    );
     if (workspace.workspaceFolders === undefined) {
         // no workspace opened
-        const { disableLanguages } = getConfig();
-        const languageSelector = allLanguages.filter(
-            l => !disableLanguages.includes(l)
-        );
-        const rangeLanguageSelector = allRangeLanguages.filter(
-            l => !disableLanguages.includes(l)
-        );
-        return { languageSelector, rangeLanguageSelector };
+        return {
+            languageSelector: globalLanguageSelector,
+            rangeLanguageSelector: globalRangeLanguageSelector,
+        };
     }
-    // at least 1 workspace
 
-    // TODO: untitled files to concat.
+    // at least 1 workspace
+    const untitledLanguageSelector: DocumentFilter[] = globalLanguageSelector.map(
+        l => ({ language: l, scheme: 'untitled' })
+    );
+    const untitledRangeLanguageSelector: DocumentFilter[] = globalRangeLanguageSelector.map(
+        l => ({ language: l, scheme: 'untitled' })
+    );
     return workspace.workspaceFolders.reduce(
         (
-            ret: {
-                languageSelector: DocumentFilter[];
-                rangeLanguageSelector: DocumentFilter[];
-            },
-            wf
+            previous,
+            workspaceFolder
         ) => {
-            const { disableLanguages } = getConfig(wf.uri);
-            let { languageSelector, rangeLanguageSelector } = ret;
-            const relativePattern = new RelativePattern(wf, '**');
-
-            languageSelector = languageSelector.concat(
-                allLanguages.filter(l => !disableLanguages.includes(l)).map(
-                    l =>
-                        ({
-                            language: l,
-                            pattern: relativePattern,
-                        } as DocumentFilter)
-                )
-            );
-            rangeLanguageSelector = rangeLanguageSelector.concat(
-                allRangeLanguages
-                    .filter(l => !disableLanguages.includes(l))
-                    .map(
-                        l =>
-                            ({
-                                language: l,
-                                pattern: relativePattern,
-                            } as DocumentFilter)
-                    )
-            );
-            return { languageSelector, rangeLanguageSelector };
+            let { languageSelector, rangeLanguageSelector } = previous;
+            const select = selectorsCreator(workspaceFolder);
+            return {
+                languageSelector: languageSelector.concat(
+                    select.languageSelector
+                ),
+                rangeLanguageSelector: rangeLanguageSelector.concat(
+                    select.rangeLanguageSelector
+                ),
+            };
         },
-        { languageSelector: [], rangeLanguageSelector: [] }
+        {
+            languageSelector: untitledLanguageSelector,
+            rangeLanguageSelector: untitledRangeLanguageSelector,
+        }
     );
 }
 
 export function activate(context: ExtensionContext) {
     const { fileIsIgnored } = ignoreFileHandler(context.subscriptions);
     const editProvider = new EditProvider(fileIsIgnored);
-
-    const { languageSelector, rangeLanguageSelector } = selectors();
-    context.subscriptions.push(
-        languages.registerDocumentRangeFormattingEditProvider(
+    function registerFormatter() {
+        disposeHandlers();
+        const { languageSelector, rangeLanguageSelector } = selectors();
+        formatterHandler = languages.registerDocumentRangeFormattingEditProvider(
             rangeLanguageSelector,
             editProvider
-        ),
-        languages.registerDocumentFormattingEditProvider(
+        );
+        rangeFormatterHandler = languages.registerDocumentFormattingEditProvider(
             languageSelector,
             editProvider
-        ),
+        );
+    }
+    registerFormatter();
+    context.subscriptions.push(
+        workspace.onDidChangeWorkspaceFolders(registerFormatter),
+        {
+            dispose: disposeHandlers,
+        },
         setupErrorHandler(),
         configFileListener(),
         ...registerDisposables()
