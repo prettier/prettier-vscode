d898930b396b5bfcd336195784c67147a3bf8efc
svipas <benas.svipas@gmail.com>
2019-07-08 17:12:55 +0300

    Fix runTest

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -4,15 +4,15 @@ import { runTests } from 'vscode-test';
 (async function main() {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
-    const extensionPath = process.cwd();
+    const extensionDevelopmentPath = process.cwd();
 
     // The path to test runner
     // Passed to --extensionTestsPath
-    const testRunnerPath = path.join(__dirname, './suite');
+    const extensionTestsPath = path.join(__dirname, './suite');
 
     // The path to the workspace file
     const testWorkspace = path.resolve('testWorkspace.code-workspace');
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionPath, testRunnerPath, testWorkspace });
+    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace] });
 })();
