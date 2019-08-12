b31fbe057e1eec02f20b8a055d12954f4a23b7f7
Nathan Totten <ntotten@salesforce.com>
2019-08-12 11:44:45 -0400

    Added config file tests

---
+++ src/test/suite/config.test.ts
@@ -0,0 +1,32 @@
+import * as assert from 'assert';
+import { workspace } from 'vscode';
+import { format } from './format.test';
+
+const testConfig = (filePath: string) => {
+    return async () => {
+        const { result } = await format(
+            filePath,
+            workspace.workspaceFolders![5].uri
+        );
+        assert.equal(
+            result,
+            `function foo() {
+   console.log("test");
+}
+`
+        );
+    };
+};
+
+suite('Test configurations', function() {
+    this.timeout(10000);
+    test('it uses config from .prettierrc file ', testConfig('rcfile/test.js'));
+    test(
+        'it uses config from prettier.config.js file ',
+        testConfig('jsconfigfile/test.js')
+    );
+    test(
+        'it uses config from .prettierrc.js file ',
+        testConfig('jsfile/test.js')
+    );
+});
---
+++ test-fixtures/config/jsfile/.prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+   tabWidth: 3
+};
