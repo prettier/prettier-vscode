f7afb35f1d1eed1a0113a667662e5a95e0dc5016
Nathan Totten <nathan@ntotten.com>
2020-04-05 10:52:58 -0400

    removed test skips for windows

--- src/test/suite/eslint.test.ts
+++ src/test/suite/eslint.test.ts
@@ -12,9 +12,6 @@ suite("Test eslint", function () {
   this.beforeAll(moveRootPrettierRC);
   this.afterAll(putBackPrettierRC);
   test("it formats with prettier-eslint", async () => {
-    if (platform() === "win32") {
-      return assert.ok(true, "Skipping test on windows.");
-    }
     const { actual } = await format("eslint", "index.js");
     const expected = await getText("eslint", "index.result.js");
     return assert.equal(actual, expected);
--- src/test/suite/tslint.test.ts
+++ src/test/suite/tslint.test.ts
@@ -12,9 +12,6 @@ suite("Test tslint", function () {
   this.beforeAll(moveRootPrettierRC);
   this.afterAll(putBackPrettierRC);
   test("it formats with prettier-tslint", async () => {
-    if (platform() === "win32") {
-      return assert.ok(true, "Skipping test on windows.");
-    }
     const { actual } = await format("tslint", "index.ts");
     const expected = await getText("tslint", "index.result.ts");
     assert.equal(actual, expected);
