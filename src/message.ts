58e4c75d84efb92577a487cea311446d6f933f4f
Jacob Hilker <hilker.j@gmail.com>
2020-04-27 18:11:35 -0400

    Add custom error message for invalid prettierPath

--- src/message.ts
+++ src/message.ts
@@ -1,5 +1,7 @@
 export const OUTDATED_PRETTIER_VERSION_MESSAGE =
   "Your project is configured to use an outdated version of prettier that cannot be used by this extension. Upgrade to the latest version of prettier.";
+export const INVALID_PRETTIER_PATH_MESSAGE =
+  "`prettierPath` option does not reference a valid instance of Prettier. Please ensure you are passing a path to the prettier module, not the binary. Falling back to bundled version of prettier.";
 export const VIEW_LOGS_ACTION_TEXT = "Show Log";
 export const FAILED_TO_LOAD_MODULE_MESSAGE =
   "Failed to load module. If you have prettier or plugins referenced in package.json, ensure you have run `npm install`";
