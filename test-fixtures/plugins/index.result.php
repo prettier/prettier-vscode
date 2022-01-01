b34ce688ec8502d9e176ea016052bd54e599078c
Nate Totten <nate@zuplo.com>
2022-01-01 16:51:47 +0000

    prettier to 2.5.1

--- test-fixtures/plugins/index.result.php
+++ test-fixtures/plugins/index.result.php
@@ -4,11 +4,11 @@ array_map(
   function ($arg1, $arg2) use ($var1, $var2) {
     return $arg1 + $arg2 / ($var + $var2);
   },
-  array(
+  [
     "complex" => "code",
     "with" => "inconsistent",
     "formatting" => "is",
     "hard" => "to",
-    "maintain" => true
-  )
+    "maintain" => true,
+  ]
 );
