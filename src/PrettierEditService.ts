be790ca13b85b26196340ce2a89a1f8a47545605
Dan Rose <dan@digilabs.io>
2022-03-28 19:04:51 -0500

    fix "editor.formatOnSaveMode": "modifications" (#2447)
    * fix "editor.formatOnSaveMode": "modifications"
    
    * Add test for formatting multiple ranges
    
    * remove leftover cruft

--- src/PrettierEditService.ts
+++ src/PrettierEditService.ts
@@ -323,8 +323,36 @@ export default class PrettierEditService implements Disposable {
     this.loggingService.logInfo(
       `Formatting completed in ${duration / 1000}ms.`
     );
-    return [TextEdit.replace(this.fullDocumentRange(document), result)];
+    const edit = this.minimalEdit(document, result);
+    return [edit];
   };
+
+  private minimalEdit(document: TextDocument, string1: string) {
+    const string0 = document.getText();
+    // length of common prefix
+    let i = 0;
+    while (
+      i < string0.length &&
+      i < string1.length &&
+      string0[i] === string1[i]
+    ) {
+      ++i;
+    }
+    // length of common suffix
+    let j = 0;
+    while (
+      i + j < string0.length &&
+      i + j < string1.length &&
+      string0[string0.length - j - 1] === string1[string1.length - j - 1]
+    ) {
+      ++j;
+    }
+    const newText = string1.substring(i, string1.length - j);
+    const pos0 = document.positionAt(i);
+    const pos1 = document.positionAt(string0.length - j);
+
+    return TextEdit.replace(new Range(pos0, pos1), newText);
+  }
 
   /**
    * Format the given text with user's configuration.
@@ -506,10 +534,5 @@ export default class PrettierEditService implements Disposable {
     }
 
     return options;
-  }
-
-  private fullDocumentRange(document: TextDocument): Range {
-    const lastLineId = document.lineCount - 1;
-    return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
   }
 }
