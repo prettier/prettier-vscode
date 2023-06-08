5b7bbcb2da1ce389c4c4f638e4e51712e4fdfb1f
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-06-09 03:35:48 +0900

    Skip plugins tests for only linux

--- src/test/suite/plugins.test.ts
+++ src/test/suite/plugins.test.ts
@@ -1,17 +1,23 @@
 import * as assert from "assert";
+import * as os from "os";
 import { format, getText } from "./format.test";
+
+const t = os.platform() === "linux" ? test.skip : test;
 
 suite("Test plugins", function () {
   this.timeout(10000);
-  test.skip("it formats with plugins", async () => {
+  t("it formats with plugins", async () => {
     const { actual } = await format("plugins", "index.php");
     const expected = await getText("plugins", "index.result.php");
     assert.equal(actual, expected);
   });
 
-  test.skip("it correctly resolved plugin in pnpm node_modules dirs structure", async () => {
-    const { actual } = await format("plugins-pnpm", "index.js");
-    const expected = await getText("plugins-pnpm", "index.result.js");
-    assert.equal(actual, expected);
-  });
+  t(
+    "it correctly resolved plugin in pnpm node_modules dirs structure",
+    async () => {
+      const { actual } = await format("plugins-pnpm", "index.js");
+      const expected = await getText("plugins-pnpm", "index.result.js");
+      assert.equal(actual, expected);
+    }
+  );
 });
