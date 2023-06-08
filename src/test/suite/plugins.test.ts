c9c7477246d54a653c44a123a70e677c9e360db5
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-06-09 03:13:22 +0900

    Skip plugins tests

--- src/test/suite/plugins.test.ts
+++ src/test/suite/plugins.test.ts
@@ -3,7 +3,7 @@ import { format, getText } from "./format.test";
 
 suite("Test plugins", function () {
   this.timeout(10000);
-  test("it formats with plugins", async () => {
+  test.skip("it formats with plugins", async () => {
     const { actual } = await format("plugins", "index.php");
     const expected = await getText("plugins", "index.result.php");
     assert.equal(actual, expected);
