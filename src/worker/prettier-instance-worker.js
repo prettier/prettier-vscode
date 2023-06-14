f1cda05d26329892b0e9a7c94b6de1544af22c93
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-06-14 23:21:52 +0900

    Requite instance while calling method if instance does not exist

--- src/worker/prettier-instance-worker.js
+++ src/worker/prettier-instance-worker.js
@@ -2,53 +2,48 @@ const { parentPort } = require("worker_threads");
 
 const path2ModuleCache = new Map();
 
+function requireInstance(modulePath) {
+  let prettierInstance = path2ModuleCache.get(modulePath);
+  if (!prettierInstance) {
+    prettierInstance = require(modulePath);
+    if (!prettierInstance.format) {
+      throw new Error("wrong instance");
+    }
+    path2ModuleCache.set(modulePath, prettierInstance);
+  }
+  return prettierInstance;
+}
+
 parentPort.on("message", ({ type, payload }) => {
   switch (type) {
     case "import": {
       const { modulePath } = payload;
-      let prettierInstance = path2ModuleCache.get(modulePath);
-      if (!prettierInstance) {
-        try {
-          prettierInstance = require(modulePath);
-          // If the instance is missing `format`, it's probably
-          // not an instance of Prettier
-          if (!prettierInstance.format) {
-            parentPort.postMessage({
-              type,
-              payload: { version: null },
-            });
-            break;
-          }
-          path2ModuleCache.set(modulePath, prettierInstance);
-        } catch {
-          parentPort.postMessage({
-            type,
-            payload: { version: null },
-          });
-          break;
-        }
+      try {
+        const prettierInstance = requireInstance(modulePath);
+        parentPort.postMessage({
+          type,
+          payload: { version: prettierInstance.version },
+        });
+      } catch {
+        parentPort.postMessage({
+          type,
+          payload: { version: null },
+        });
       }
-      parentPort.postMessage({
-        type,
-        payload: { version: prettierInstance.version },
-      });
       break;
     }
     case "callMethod": {
       const { modulePath, methodName, methodArgs, id } = payload;
-      const prettierInstance = path2ModuleCache.get(modulePath);
+      let prettierInstance = path2ModuleCache.get(modulePath);
       if (!prettierInstance) {
-        parentPort.postMessage({
-          type,
-          payload: {
-            result: new Error(
-              "Cannot load prettier instance from cache. Please call import() before call callMethod."
-            ),
-            id,
-            isError: true,
-          },
-        });
-        break;
+        try {
+          prettierInstance = requireInstance(modulePath);
+        } catch (error) {
+          parentPort.postMessage({
+            type,
+            payload: { result: error, id, isError: true },
+          });
+        }
       }
       let result;
       try {
