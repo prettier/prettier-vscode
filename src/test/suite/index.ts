3ffe29be24ca1ee9aff4e45ed68fe31de8554966
Nathan Totten <nate@zuplo.com>
2025-11-28 10:38:27 -0500

    updated esbuild

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,8 +1,8 @@
-import * as glob from "glob";
+import { glob } from "node:fs/promises";
 import * as Mocha from "mocha";
 import * as path from "path";
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: "tdd",
@@ -11,30 +11,25 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, "..");
 
+  for await (const file of glob("**/*.test.js", { cwd: testsRoot })) {
+    mocha.addFile(path.resolve(testsRoot, file));
+  }
+
+  // To run only a single test, set this value
+  // mocha.grep("<test name>");
+
   return new Promise((c, e) => {
-    glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return e(err);
-      }
-
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-
-      // To run only a single test, set this value
-      // mocha.grep("<test name>");
-
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (error) {
-        e(error);
-      }
-    });
+    try {
+      // Run the mocha test
+      mocha.run((failures) => {
+        if (failures > 0) {
+          e(new Error(`${failures} tests failed.`));
+        } else {
+          c();
+        }
+      });
+    } catch (error) {
+      e(error);
+    }
   });
 }
